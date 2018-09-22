50646d02678c717bd3804efa6ce6c22e173207b8
sarah <sarahbruggisser@outlook.com>
2018-09-22 11:58:14 +1000

    added hack event type

--- routes.py
+++ routes.py
@@ -1,23 +1,22 @@
 from flask import render_template, request, redirect, url_for, abort
 from flask_login import login_required, current_user, logout_user
 
-from server import app, system, auth_manager
+from server import app, system
 
 @app.route('/')
-    def calendar():
-
+def calendar():
+	pass
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if system.login_customer(username, password):
-            return redirect(url_for('calendar'))
-    return render_template('login.html')
-
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		if system.login_customer(username, password):
+			return redirect(url_for('calendar'))
+		return render_template('login.html')
 @app.route('/logout')
 @login_required
 def logout():
 	logout_user()
-    return redirect(url_for('calendar'))
+	return redirect(url_for('calendar'))
 
