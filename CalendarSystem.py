61fa36c8a0e559c0fcc9bdfb2d5a82399c0a639e
Lilian Wang <lilian.wang1712@gmail.com>
2018-09-22 11:29:28 +1000

    fixed logout

--- CalendarSystem.py
+++ CalendarSystem.py
@@ -1,10 +1,19 @@
+from User import User
+from Event import Event
+
 class CalendarSystem:
 	def __init__(self):
 		self._users = []
 		self._events = []
 
+	def get_user(self, username):
+		for user in self._users:
+			if username == user._username:
+				return user
+
 	def add_user(self, usernamee, password):
-		pass
+		user = User(username, password)
+		self.users.append(user)
 
 	def delete_user(self, user)
 		pass
@@ -14,4 +23,9 @@ class CalendarSystem:
 
 	def delete_event(self, event):
 		pass
+
+	def login(self, username, password):
+		for user in self._users:
+			if username=
+
 
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, abort
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, logout_user
 
 from server import app, system, auth_manager
 
@@ -18,7 +18,7 @@ def login():
 @app.route('/logout')
 @login_required
 def logout():
-    auth_manager.logout()
-    return redirect(url_for('home'))
+	logout_user()
+    return redirect(url_for('calendar'))
 
 
