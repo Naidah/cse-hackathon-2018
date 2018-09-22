a0d4d7b71e3b428e1a00721429b9c747a542a51a
Lilian Wang <lilian.wang1712@gmail.com>
2018-09-22 11:11:56 +1000

    added some backend

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from CalendarSystem import CalendarSystem
 
 app = Flask(__name__)
 app.secret_key = 'very-secret-123'  # Used to add entropy
@@ -9,3 +10,5 @@ app.secret_key = 'very-secret-123'  # Used to add entropy
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+system = CalendarSystem()
