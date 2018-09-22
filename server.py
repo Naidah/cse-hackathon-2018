35c8d6f17ba54197a338975dec838aa40a237d66
Lilian Wang <lilian.wang1712@gmail.com>
2018-09-22 10:49:15 +1000

    Fixed basic flask files

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_login import LoginManager
-from src.AuthenticationManager import AuthenticationManager
-from src.client import bootstrap_system
 
 app = Flask(__name__)
 app.secret_key = 'very-secret-123'  # Used to add entropy
@@ -11,10 +9,3 @@ app.secret_key = 'very-secret-123'  # Used to add entropy
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-auth_manager = AuthenticationManager(login_manager)
-system = bootstrap_system(auth_manager)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return system.get_user_by_id(user_id)
