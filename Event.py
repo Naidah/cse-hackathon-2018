e952614b669a10856589fb17f7ee6129dcb9fe71
Lilian Wang <lilian.wang1712@gmail.com>
2018-09-22 11:24:06 +1000

    Added user and event classes

---
+++ Event.py
@@ -0,0 +1,27 @@
+class Event():
+	def __init__(self, name, date, time, location, tags):
+		self._name = name
+		self._date = date
+		self._time = time
+		self._location = location
+		self._tags = tags
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def date(self):
+		return self._date
+
+	@property
+	def time(self):
+		return self._time
+
+	@property
+	def location(self):
+		return self._location
+
+	@property
+	def tags(self):
+		return self._tags
---
+++ User.py
@@ -0,0 +1,12 @@
+class User(UserMixin, ABC):
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def password(self)
+        return self._password
