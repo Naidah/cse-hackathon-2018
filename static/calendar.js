36a3ec4b10f7a6a2fae84a9b3fd8e4e105f4bda4
Naidah <aidanwhunt@gmail.com>
2018-09-22 14:02:32 +1000

    Autogenerated contents of the calendar to depend on the month

--- static/calendar.js
+++ static/calendar.js
@@ -1,16 +1,30 @@
-window.onload = function() {
-    t = document.getElementById("calendar");
-    for (let i = 0; i < 6; i++) {
-        let newRow = document.createElement("tr");
-        for (let j = 0; j < 7; j++) {
-            let newCell = document.createElement("td");
-            let cellInfo = document.createElement("div");
-            if (Math.random() > 0.8) {
-                cellInfo.innerHTML = "Event here";
-            }
-            newCell.appendChild(cellInfo);
-            newRow.appendChild(newCell);
-        }
-        t.appendChild(newRow);
+var month = 0
+var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var firstDay = 5;
+
+function setMonth(newMonth) {
+    var d = new Date();
+    month = newMonth;
+    document.getElementById("monthLabel").innerHTML = monthNames[month];
+}
+
+function initialiseCells(row, col) {
+    var date = (row*7 + col) - firstDay + 1;
+    var prevMonth = (month-1)%12;
+    var nextMonth = (month+1)%12;
+    var thisMonth = month
+    var currCell = document.getElementById("cell-"+row.toString()+"-"+col.toString());
+
+    if (date < 1) {
+        date = monthDays[prevMonth] + date;
+        thisMonth = prevMonth;
+    } else if (date > monthDays[month]) {
+        date = date % monthDays[month];
+        thisMonth = nextMonth;
     }
+
+    var content = document.createElement("div")
+    content.innerHTML = date.toString();
+    currCell.appendChild(content)
 }
